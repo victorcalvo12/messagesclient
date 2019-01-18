d04bf4e79c7fdba624400e86f09a0b2a46260802
Victor Calvo <aelius@pop-os.localdomain>
2019-01-17 18:20:06 -0600

    Window State Management

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 const { app, BrowserView, BrowserWindow } = require('electron')
+const appConfig = require('electron-settings')
+
+function windowStateKeeper(windowName) {
+    let window, windowState
+
+    function setBounds() {
+        if (appConfig.has(`windowState.${windowName}`)) {
+            windowState = appConfig.get(`windowState.${windowName}`)
+        } else {
+            windowState = {
+                x: undefined,
+                y: undefined,
+                width: 1000,
+                height: 800
+            }
+        }
+    }
+
+    function saveState() {
+        if (!windowState.isMaximized) {
+            windowState = window.getBounds()
+        }
+        windowState.isMaximized = window.isMaximized()
+        appConfig.set(`windowState.${windowName}`, windowState)
+    }
+
+    function track(win) {
+        window = win;
+        ['resize', 'move', 'close'].forEach(event => {
+            win.on(event, saveState)
+        })
+    }
+
+    setBounds()
+
+    return {
+        x: windowState.x,
+        y: windowState.y,
+        width: windowState.width,
+        height: windowState.height,
+        isMaximized: windowState.isMaximized,
+        track
+    }
+}
 
 function createWindow() {
-    let window = new BrowserWindow({ width: 900, height: 900 })
+    const mainWindowStateKeeper = windowStateKeeper('main')
+
+    const windowOptions = {
+        title: 'Google Messages',
+        x: mainWindowStateKeeper.x,
+        y: mainWindowStateKeeper.y,
+        width: mainWindowStateKeeper.width,
+        height: mainWindowStateKeeper.height
+    }
+    
+    //let window = new BrowserWindow(windowStateKeeper('main'))
+    let window = new BrowserWindow(windowOptions)
+
+    mainWindowStateKeeper.track(window)
+
+    //let window = new BrowserWindow({ width: 1600, height: 900 })
+
+    //window.webContents.openDevTools()
 
     window.on('closed', () => {
         window = null
     })
-
-    window.webContents.openDevTools()
 
     //window.loadFile('index.html')
 
@@ -16,13 +75,16 @@ function createWindow() {
             nodeIntegration: false
         }
     })
+    view.setAutoResize({width:true, height: true})
 
     window.setBrowserView(view)
     view.setBounds({
         x: 0, y: 0,
-        width: 900, height: 900
+        width: windowOptions.width, height: windowOptions.height
     })
     view.webContents.loadURL('https://messages.android.com')
+    //view.webContents.openDevTools()
+    //view.webContents. //inject = require('./script.js')
 }
 
 app.on('ready', createWindow)
