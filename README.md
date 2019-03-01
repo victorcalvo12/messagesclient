7b56e6075146a0f6915285d83178406695ebc444
Victor Calvo <aelius@pop-os.localdomain>
2019-02-28 20:13:58 -0600

    Modified git ignore, readme, and index.js

---
+++ README.md
@@ -0,0 +1,7 @@
+Google Message Desktop Client
+=============================
+
+A simple desktop client/wrapper for Google's Messages application.
+
+DISCLAIMER: I have no affiliation with Google. This app was created for my own personal use.
+Use at your own risk!
--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const { app, BrowserView, BrowserWindow } = require('electron')
-const appConfig = require('electron-settings')
+const { app, BrowserView, BrowserWindow } = require('electron');
+const appConfig = require('electron-settings');
 
 function windowStateKeeper(windowName) {
-    let window, windowState
+    let window, windowState;
 
+    //Applies saved window state to window on startup if one exists.
     function setBounds() {
         if (appConfig.has(`windowState.${windowName}`)) {
-            windowState = appConfig.get(`windowState.${windowName}`)
+            windowState = appConfig.get(`windowState.${windowName}`);
         } else {
             windowState = {
                 x: undefined,
                 y: undefined,
                 width: 1000,
                 height: 800
-            }
+            };
         }
     }
-
+    //Saves application bounds so that the app opens to its previous dimensions.
     function saveState() {
         if (!windowState.isMaximized) {
-            windowState = window.getBounds()
+            windowState = window.getBounds();
         }
-        windowState.isMaximized = window.isMaximized()
-        appConfig.set(`windowState.${windowName}`, windowState)
+        windowState.isMaximized = window.isMaximized();
+        appConfig.set(`windowState.${windowName}`, windowState);
     }
 
     function track(win) {
@@ -32,7 +33,7 @@ function windowStateKeeper(windowName) {
         })
     }
 
-    setBounds()
+    setBounds();
 
     return {
         x: windowState.x,
@@ -48,19 +49,16 @@ function createWindow() {
     const mainWindowStateKeeper = windowStateKeeper('main')
 
     const windowOptions = {
-        title: 'Google Messages',
+        title: 'Messages',
         x: mainWindowStateKeeper.x,
         y: mainWindowStateKeeper.y,
         width: mainWindowStateKeeper.width,
         height: mainWindowStateKeeper.height
     }
     
-    //let window = new BrowserWindow(windowStateKeeper('main'))
-    let window = new BrowserWindow(windowOptions)
+    let window = new BrowserWindow(windowOptions);
 
-    mainWindowStateKeeper.track(window)
-
-    //let window = new BrowserWindow({ width: 1600, height: 900 })
+    mainWindowStateKeeper.track(window);
 
     //window.webContents.openDevTools()
 
@@ -68,23 +66,21 @@ function createWindow() {
         window = null
     })
 
-    //window.loadFile('index.html')
-
     let view = new BrowserView({
         webPreferences: {
             nodeIntegration: false
         }
-    })
-    view.setAutoResize({width:true, height: true})
+    });
+    view.setAutoResize({width:true, height: true});
 
-    window.setBrowserView(view)
+    window.setBrowserView(view);
     view.setBounds({
         x: 0, y: 0,
         width: windowOptions.width, height: windowOptions.height
     })
-    view.webContents.loadURL('https://messages.android.com')
+    view.webContents.loadURL('https://messages.android.com');
+    
     //view.webContents.openDevTools()
-    //view.webContents. //inject = require('./script.js')
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
